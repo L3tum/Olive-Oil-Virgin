04582b08dc40be93dbf6abdb768a61adb527ff8b
Letum <tomn.pauly@gmail.com>
2015-06-02 15:10:25 +0200

    Better way for loading messages
    Cleaned up code

--- SlacksBot/SlacksBot.py
+++ SlacksBot/SlacksBot.py
@@ -7,41 +7,43 @@ import yaml
 import json
 import logging
 
-myToken = "xoxp-5007212458-5029573905-5127214062-7f9749"
 botToken = "xoxb-5134150563-itkh6B9inXzlkrCWaBdkCeLS"
 itrID = "U053KLMS7"
 channelRandom = "C050768H2"
 output = []
 Matrix = [[0 for x in range(5)] for x in range(5)]
+
+#currently the max numbers of things in Matrix are 5, change that by changing above range(number)
+MatrixLength = 5
 bitteWendenId = "U0539QRF3"
 
 sc = SlackClient(botToken)
 if sc.rtm_connect():
     #adds the outputs
     Matrix[0][0] = "Hi"
-    Matrix[1][0] = "Hello"
+    Matrix[0][1] = "Hello"
 
-    #sc.rtm_send_message(channelRandom, "Hi")
+    #get Authentification
     dat = sc.api_call("api.test")
     dat2 = sc.api_call("auth.test", token = botToken)
+    print dat
+    print "--------"
+    print dat2
 
 
     while True:
-        #sc.api_call("users.setPresence", token = botToken, presence = "away")
+        sc.api_call("users.setPresence", token = botToken, presence = "away")
 
         for data in sc.rtm_read():
-            #print data["type"]
-            print sc.server.users
             if "type" in data:
                if data["type"] == "message":
                   chanel = data["channel"]
-                  text = data["text"]
-                  if text == Matrix[0][0]:
-                     sc.api_call("chat.postMessage", channel = chanel, text = "Hello <@{}|{}> <@{}|bittewenden>".format(sc.server.users.find(data["user"]).id, sc.server.users.find(data["user"]).name, bitteWendenId), as_user = True, parse = True)
-                #if data["type"] == "user_typing":
-                #   output.append("@{} is typing a message".format(sc.server.users.find(data["user"]).name))
-                #   sc.rtm_send_message(data["channel"], output[0])
-        
+                  if "text" in data:
+                     text = data["text"]
+                     for x in range(0, MatrixLength):
+                         if text == Matrix[x][0]:
+                            sc.api_call("chat.postMessage", channel = chanel, text = "{} <@{}|{}>".format(Matrix[x][1], sc.server.users.find(data["user"]).id, sc.server.users.find(data["user"]).name, bitteWendenId), as_user = True, parse = True)
+
         time.sleep(1)
 else:
     print "Connection Failed, invalid token?"
